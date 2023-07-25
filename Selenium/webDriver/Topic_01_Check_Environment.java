4d3d21c717a4036650369449775768e88eaf8108
hien95 <nhihien400@gmail.com>
2023-07-25 16:52:07 +0700

    update

--- Selenium/webDriver/Topic_01_Check_Environment.java
+++ Selenium/webDriver/Topic_01_Check_Environment.java
@@ -7,7 +7,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
 
 public class Topic_01_Check_Environment {
